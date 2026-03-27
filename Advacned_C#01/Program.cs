6646ffadd1bef4a4ec465402e0fa84bc6d7ba716
mahmoudahmed13 <mahmoudahmed13@gmail.com>
2026-03-28 00:56:23 +0200

    Question02

--- Advacned_C#01/Program.cs
+++ Advacned_C#01/Program.cs
@@ -12,6 +12,16 @@ namespace Advacned_C_01
 
             //A generic class uses type parameters that are replaced with actual types when you create an instance.The type parameter T acts as a placeholder.
             #endregion
+
+            #region Q2: Write a generic class Container<T> with Add and Get methods
+            //internal class Container<T>
+            //{
+            //    private readonly List<T> _containers = [];
+            //    public void Add(T item) => _containers.Add(item);
+
+            //    public List<T> GetValues() => _containers;
+            //}
+            #endregion
         }
     }
 }

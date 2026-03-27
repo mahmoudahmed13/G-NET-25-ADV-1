f897f5e2fa8bb6a6ab0cddca409699e3a7615a65
mahmoudahmed13 <mahmoudahmed13@gmail.com>
2026-03-28 00:40:09 +0200

    Question01

--- Advacned_C#01/Program.cs
+++ Advacned_C#01/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Advacned_C_01
+﻿using System.Reflection.Metadata;
+
+namespace Advacned_C_01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region Q1: What is a generic class? Why use generics?
+            //allow you to define type - safe classes, interfaces, methods,and delegates
+            //without committing to a specific data type until the code is used , Type Safety and preformance.
+
+            //A generic class uses type parameters that are replaced with actual types when you create an instance.The type parameter T acts as a placeholder.
+            #endregion
         }
     }
 }

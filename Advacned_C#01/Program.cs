b67de9c6fc79d58c7b56231fe97bce9eca62a383
mahmoudahmed13 <mahmoudahmed13@gmail.com>
2026-03-28 00:40:09 +0200

    Question01

--- Advacned_C#01/Program.cs
+++ Advacned_C#01/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q1: What is a generic class? Why use generics?
+            //allow you to define type - safe classes, interfaces, methods,and delegates
+            //without committing to a specific data type until the code is used , Type Safety and preformance.
+            #endregion
         }
     }
 }
